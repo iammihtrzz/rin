4516298da32fa39e723c07b43350a0a689c3aefc
Minh Tran <mihtrzz.xyz@gmail.com>
2025-04-15 16:45:54 +0700

    wink.js

---
+++ wink.js
@@ -0,0 +1,66 @@
+var __encode = 'ig:mihtrzz',
+    _a = {},
+    _0xb483 = ["_decode", "https://www.facebook.com/mihtrzz"];
+(function (_0xd642x1) {
+    _0xd642x1[_0xb483[0]] = _0xb483[1]
+})(_a);
+var __Oxfe574 = ["body", "parse", "data", "mihtrzz", "32811210727000", "", "32811210727000", "32811210727000", "mihtrzz", "4", "Wink", "32811210727000", "32811210727000", "32811210727000", "0", "stringify", "undefined", "log", "mihtrzz", "mihtrzz，jsmihtrzz", "mihtrzz，", "mihtrzz", "jsjia", "mi.com"];
+var body = $response[__Oxfe574[0x0]];
+var hausd0rff = JSON[__Oxfe574[0x1]](body);
+hausd0rff[__Oxfe574[0x2]] = {
+    "active_sub_type": 2,
+    "account_type": 1,
+    "sub_type_name": __Oxfe574[0x3],
+    "active_sub_order_id": __Oxfe574[0x4],
+    "trial_period_invalid_time": __Oxfe574[0x5],
+    "current_order_invalid_time": __Oxfe574[0x6],
+    "active_order_id": __Oxfe574[0x7],
+    "limit_type": 0,
+    "active_sub_type_name": __Oxfe574[0x3],
+    "use_vip": true,
+    "have_valid_contract": true,
+    "derive_type_name": __Oxfe574[0x8],
+    "derive_type": 1,
+    "in_trial_period": false,
+    "is_vip": true,
+    "membership": {
+        "id": __Oxfe574[0x9],
+        "display_name": __Oxfe574[0xa],
+        "level": 1,
+        "level_name": __Oxfe574[0x8]
+    },
+    "active_promotion_status_list": [0x2],
+    "sub_type": 2,
+    "account_id": __Oxfe574[0xb],
+    "invalid_time": __Oxfe574[0xc],
+    "valid_time": __Oxfe574[0xd],
+    "active_product_id": __Oxfe574[0xe],
+    "active_promotion_status": 2,
+    "show_renew_flag": true
+};
+$done({
+    body: JSON[__Oxfe574[0xf]](hausd0rff)
+});;;
+(function (_0xcbd8x3, _0xcbd8x4, _0xcbd8x5, _0xcbd8x6, _0xcbd8x7, _0xcbd8x8) {
+    _0xcbd8x8 = __Oxfe574[0x10];
+    _0xcbd8x6 = function (_0xcbd8x9) {
+        if (typeof alert !== _0xcbd8x8) {
+            alert(_0xcbd8x9)
+        };
+        if (typeof console !== _0xcbd8x8) {
+            console[__Oxfe574[0x11]](_0xcbd8x9)
+        }
+    };
+    _0xcbd8x5 = function (_0xcbd8xa, _0xcbd8x3) {
+        return _0xcbd8xa + _0xcbd8x3
+    };
+    _0xcbd8x7 = _0xcbd8x5(__Oxfe574[0x12], _0xcbd8x5(_0xcbd8x5(__Oxfe574[0x13], __Oxfe574[0x14]), __Oxfe574[0x15]));
+    try {
+        _0xcbd8x3 = __encode;
+        if (!(typeof _0xcbd8x3 !== _0xcbd8x8 && _0xcbd8x3 === _0xcbd8x5(__Oxfe574[0x16], __Oxfe574[0x17]))) {
+            _0xcbd8x6(_0xcbd8x7)
+        }
+    } catch (e) {
+        _0xcbd8x6(_0xcbd8x7)
+    }
+})({})
